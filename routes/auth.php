bb231757a2bf6312abd4369e8571588e0c788297
Laboratorynotices <muenchen.webas@gmail.com>
2021-11-24 12:17:10 +0100

    Прописаны новые пути для администрирования услуг.

--- routes/auth.php
+++ routes/auth.php
@@ -62,3 +62,6 @@ Route::post('/confirm-password', [ConfirmablePasswordController::class, 'store']
 Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])
                 ->middleware('auth')
                 ->name('logout');
+
+Route::get('/logout', [AuthenticatedSessionController::class, 'destroy'])
+                ->middleware('auth');
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,11 @@ use Illuminate\Support\Facades\Route;
 // Основной контроллер
 use App\Http\Controllers\LandingPageController;
 
+
+use App\Http\Controllers\AmenityController;
+use App\Models\Amenity;
+
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -32,5 +37,63 @@ Route::get('/welcome', function () {
 Route::get('/dashboard', function () {
 	return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
+
+// Добавляем префикс "admin/" к этим адресам
+Route::prefix('admin')
+	// пользователи должны быть авторизованными
+	->middleware(['auth'])
+	// объединяем всё в группу
+	->group(function () {
+
+	// Место для блоков услуг и "о нас"
+
+	// Добавляем префикс 'amenities'. Блок с удобствами.
+	Route::prefix('amenities')->group(function () {
+
+		// Список удобств
+		Route::get('/',
+			[AmenityController::class, 'index']
+		)->name('amenity.index');
+
+		// Форма для редактирования записи
+		Route::get('/edit/{amenity}',
+			// указание на контроллер и метод
+			[AmenityController::class, 'edit']
+		)
+		// фильтр по входящему параметру
+		->whereNumber('amenity')
+		// наименование маршрута
+		->name('amenity.edit');
+
+		// Обработка данных, полученных от формы
+		Route::post('/update/{amenity}',
+			// указание на контроллер и метод
+			[AmenityController::class, 'update']
+		)
+		// фильтр по входящему параметру
+		->whereNumber('amenity')
+		// наименование маршрута
+		->name('amenity.update');
+
+		// Форма для создания новой записи
+		Route::get('/create',
+			// указание на контроллер и метод
+			[AmenityController::class, 'create']
+		)
+		// наименование маршрута
+		->name('amenity.create');
+
+		// Обработка данных, полученных от формы создания
+		Route::post('/store',
+			// указание на контроллер и метод
+			[AmenityController::class, 'store']
+		)
+		// наименование маршрута
+		->name('amenity.store');						
+	});
+
+	// Место для остальных блоков
+
+});
 
 require __DIR__.'/auth.php';
